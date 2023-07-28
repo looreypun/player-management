c986ab879ddca18806c19867f0dba2325e8ec634
プンヒマル <himal.pun.itsherpa@gmail.com>
2023-07-28 18:59:47 +0900

    updating welcome and dashboard views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,13 @@
     <div class="container mt-5">
         <div class="d-flex justify-content-center">
             <div class="card">
-                <div class="card-header">
+                <div class="card-header bg-danger">
                     <h2 class="card-title">Welcome to the Dashboard</h2>
                 </div>
                 <div class="card-body">
                     <p class="card-text">Hello there!</p>
                     <p class="card-text">Feel free to explore the dashboard and manage your account.</p>
-                    <a href="#" class="btn btn-primary">Get Started</a>
+                    <a href="#" class="btn btn-dark">Get Started</a>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,11 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card text-center">
-                    <div class="card-header">
-                        <h2 class="text-secondary">Welcome to Redzone</h2>
+                    <div class="card-header bg-danger">
+                        <h2 class="text-light">Welcome to Redzone</h2>
                     </div>
                     <div class="card-body">
-                        <p>Hello there! This is a simple welcome page with of Redzone.</p>
+                        <p>Hello there! This is a welcome page of Redzone.</p>
                         <p>Feel free to explore the website and enjoy your stay!</p>
                     </div>
                 </div>
