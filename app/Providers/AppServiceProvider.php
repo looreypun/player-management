10e2424c6cb0254b6bd71f44c607ed7e8552055f
looreypun <looreyhunks@gmail.com>
2023-08-03 15:46:43 +0900

    AppServiceProvider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,16 @@ class AppServiceProvider extends ServiceProvider
     {
         // \URL::forceScheme('https');
         // $this->app['request']->server->set('HTTPS','on');
+
+        // Get the APP_URL from the .env file
+        $appUrl = config('app.url');
+
+        // Check if the environment has '/redzone-dev' in the APP_URL and set the base URL accordingly
+        if (!str_contains($appUrl, '/redzone-dev')) {
+            config(['app.url' => $appUrl]);
+        } else {
+            // Set the default base URL for the main application (without '/dev')
+            config(['app.url' => rtrim($appUrl, '/').'/redzone-dev']);
+        }
     }
 }
