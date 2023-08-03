f0c5cc5e579fac9d04aeab7789d08fe84c70fea6
looreypun <looreyhunks@gmail.com>
2023-08-03 11:34:09 +0900

    deploy test

---
+++ server_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'Redzone is being (quickly!) updated. Please try again in a minute.') || true
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    # php artisan migrate --force
+
+    # Clear cache
+    php artisan optimize
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
